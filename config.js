73993b4bd7a830fb6d15eb90201be330eddc6201
LT0406 <86857341+LT0406@users.noreply.github.com>
2021-07-03 05:13:40 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
+    introTitle: 'Bé iu à!',
+    introDesc: `You are the bét gift I have ever had
+    You are the most beautyful girl I have ever seen
+    You are the sexiest lady I have ever sex
+    You are my everything`,
+    btnIntro: 'Happy 400 days together',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    btnYes: 'Anh chồng ưng nhất quả đất',
+    btnNo: 'Không, chán anh lắm :(',
+    question:'Trên thế giới hơn 7 tỉ người mà sao bé lại yêu anh <3',
+    btnReply: 'Nói chồng nghe coi :3',
+    reply: 'Vì anh chồng là người thương em nhất quả đất <33333333',
+    mess: 'Ỏ, bé đáng yêu thíaaaa 🥰. Yêu bé nhiều nhiều 😘😘',
+    messDesc: 'Nay mình phải celebrate thật to nha công chúa.',
+    btnAccept: 'Dạaaaaaaaa <3',
+    messLink: 'https://www.youtube.com/watch?v=o_lN37OAJ9U' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
